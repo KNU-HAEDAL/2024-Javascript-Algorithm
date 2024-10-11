1e2b277d917b548303949cef081aac9a7634825f
Dobbymin <gmin0701@knu.ac.kr>
2024-10-11 20:42:14 +0900

    Solve: 문제 15 - 요세푸스 문제

---
+++ Dobbymin/Algorithm/book/15.js
@@ -0,0 +1,36 @@
+class Queue {
+	items = [];
+	front = 0;
+	rear = 0;
+
+	push(item) {
+		this.items.push(item);
+		this.rear++;
+	}
+
+	size() {
+		return this.rear - this.front;
+	}
+
+	pop() {
+		return this.items[this.front++];
+	}
+}
+
+function solution(n, k) {
+	const queue = new Queue();
+	for (let i = 1; i <= n; i++) {
+		queue.push(i);
+	}
+
+	while (queue.size() > 1) {
+		for (let i = 1; i < k; i++) {
+			queue.push(queue.pop());
+		}
+		queue.pop();
+	}
+
+	return queue.pop();
+}
+
+console.log(solution(5, 2));
